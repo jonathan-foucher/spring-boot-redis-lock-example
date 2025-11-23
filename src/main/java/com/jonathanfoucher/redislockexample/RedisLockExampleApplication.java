ea577e91e40e2a7a0403b01180a976f12b23d3ca
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-11-23 14:26:08 +0100

    chore: upgrade dependencies and java versions

--- src/main/java/com/jonathanfoucher/redislockexample/RedisLockExampleApplication.java
+++ src/main/java/com/jonathanfoucher/redislockexample/RedisLockExampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisLockExampleApplication {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(RedisLockExampleApplication.class, args);
     }
 }
